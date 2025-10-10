02bbf74f649437c472b46bb0fc8b0440a6260495
Amit9826 <suryawanshiamit492@gmail.com>
2025-10-10 16:30:05 +0530

    today update

--- domm.js
+++ domm.js
@@ -1,11 +1,26 @@
-  let num = 0;
+//   let num = 0;
+//   function increase() {
+//     num = num + 1;  
+//     document.getElementById("count").textContent = num;
+//   }
+//   function decrease() {
+//     if (num > 0) { 
+//       num = num - 1; 
+//       document.getElementById("count").textContent = num;
+//     }
+// }
+
+
+
+ var count = document.getElementById("count")
+let num = 0;
   function increase() {
-    num = num + 1;  
-    document.getElementById("count").textContent = num;
+    count.innerHTML = ++num;
   }
   function decrease() {
     if (num > 0) { 
-      num = num - 1; 
-      document.getElementById("count").textContent = num;
+      count.innerHTML = --num;
+    }else{
+      alert("not less then 0")
     }
 }
